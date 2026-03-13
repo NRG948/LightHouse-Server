23ba9293e49c786a20a57f3b206893151bbb9d49
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2026-03-12 19:02:23 -0700

    Update MatchDatabase.java

--- src/main/java/com/nrg948/tba/MatchDatabase.java
+++ src/main/java/com/nrg948/tba/MatchDatabase.java
@@ -5,9 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface MatchDatabase extends JpaRepository<MatchEntry, Long> {
+public interface MatchDatabase extends JpaRepository<MatchEntry, String> {
 	Optional<MatchEntry> findByEventKeyAndMatchNumberAndCompLevel(
-	    String keyy, 
-	    int matchNumber, 
-	    String compLevel);
+		String keyy, 
+		int matchNumber, 
+		String compLevel);
 }
